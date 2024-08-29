217669630cd39e7da73b05274f4a4826ad177e0f
cj0003 <metalliccj@gmail.com>
2024-08-29 17:16:42 +0200

    closing done

--- box_extract.cpp
+++ box_extract.cpp
@@ -4,6 +4,7 @@
 #include<iostream>
 #include <filesystem>
 #include <opencv2/opencv.hpp>
+#include <cmath>
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -11,7 +12,7 @@ using namespace cv;
 
 
 Mat grayImage, bluredImage, detected_edges, final;
-int lowThreshold = 80, upperThreshold;
+int lowThreshold = 100, upperThreshold;
 std::vector<cv::Point> polygon_corners;
 
 static Mat CannyThreshold(Mat img)
@@ -28,39 +29,55 @@ GaussianBlur( grayImage, bluredImage, Size( 3,3), 0, 0 );
 
 Canny( bluredImage, detected_edges, lowThreshold, lowThreshold*3);
 
-// final = Scalar::all(0);
-// colorImage.copyTo( final, detected_edges);
-// imshow( window_name, final );
-// imshow( window_name, detected_edges );
-
 return detected_edges;
 }
 
 
-void masking(Mat image,int i)
+static Mat masking(Mat image,int i)
 {
     Mat masked_image;
 
     //create a black image of the same size as the original image
         Mat blackimg = Mat::zeros(image.size(),CV_8UC1);
-        imshow("Blackimage" + to_string(i),blackimg);
+        // imshow("Blackimage" + to_string(i),blackimg);
 
     //taking the polygon area (hard coding)
         polygon_corners.push_back(cv::Point(243, 46));  
-        polygon_corners.push_back(cv::Point(611, 699)); 
-        polygon_corners.push_back(cv::Point(912, 616)); 
-        polygon_corners.push_back(cv::Point(408, 17));  
+        polygon_corners.push_back(cv::Point(614, 718)); 
+        polygon_corners.push_back(cv::Point(933, 585)); 
+        polygon_corners.push_back(cv::Point(412, 19));  
         
 
         fillPoly(blackimg, polygon_corners, Scalar(255, 255, 255)); //fill the mask with white
-        imshow("Blackimage filled" + to_string(i),blackimg);
+        // imshow("Blackimage filled" + to_string(i),blackimg);
 
         //applyinng the mask to the original image
         bitwise_and(image,image,masked_image,blackimg);
         imshow("And Mask Applied" + to_string(i),masked_image);
-        bitwise_not(image,masked_image,blackimg);
-        imshow("Not Mask Applied" + to_string(i),masked_image); //using the not mask also removes the white lines into black
+        // bitwise_not(image,masked_image,blackimg);
+        // imshow("Not Mask Applied" + to_string(i),masked_image); //using the not mask also removes the white lines into black
+
+        return masked_image;
         
+}
+
+static Mat findlines(Mat img,int i)
+{
+    Mat finallines,grayImage;
+    
+    //step1: convert to grayscale 
+    cvtColor(img, grayImage, COLOR_BGR2GRAY);
+    imshow("Grayimage" + to_string(i),grayImage);
+    // Step 2: applying threshhold
+    threshold(grayImage, grayImage,130 , 255, cv::THRESH_BINARY); 
+    // Step 3: bluring
+    medianBlur(grayImage,grayImage,5);
+    imshow("gray blur image",grayImage);
+
+
+    grayImage.copyTo(finallines);
+    return finallines;
+
 }
 
 
@@ -85,20 +102,62 @@ int main() {
         
         imshow("image" + to_string(i),image);
 
-        masking(image,i);
+        Mat masked = masking(image,i);
 
         
-
+        
         
 
+        // morphologyEx( final, final, MORPH_CLOSE, cv::getStructuringElement(MORPH_RECT,Size(5,5) )); 
+
+        //detecting edges using the morphological operators
+        morphologyEx( masked, final,MORPH_GRADIENT, Mat()); //works better
+        threshold(final,final,100, 200, cv::THRESH_BINARY); 
+        
+        imshow("Closedimage" + to_string(i),final); 
+
+        //now we use erosion and dilation to make the output more prominent     
+        dilate(final,final,Mat());
+        imshow("Dilatedimage" + to_string(i),final); 
+
+        erode(final,final,Mat());
+        imshow("Erodedimage" + to_string(i),final);  
+
+        //uncomment for canny
         //calling Canny
-        final = CannyThreshold(image);
-        imshow("Cannyimage" + to_string(i),final);
+        // final = CannyThreshold(final);
+        // imshow("Cannyimage" + to_string(i),final);
 
+        Canny( final, detected_edges, lowThreshold, 250);
+        imshow("Cannyimage" + to_string(i),detected_edges); //calling canny directly
 
+        // Probabilistic Hough Line Transform
+        vector<Vec4f> lines; // will hold the results of the detection
+        // HoughLinesP(final, lines, 10, CV_PI/180, 50, 100, 70 ); 
+
+        // for(int i =0; i<(int)lines.size();i++)
+        // {
+        // //finding slope using using y2 - y1/x2-x1
+        // double slope = (lines[i][3] - lines[i][1]) / (lines[i][2] - lines[i][0]);
+        // double angle = atan(slope);
+        // angle = angle * 180 / M_PI;
         
+        // //finding arc tan of the slope gives us the angle
+
+        // if(angle>=50 && angle<=70)
+        // {
+        //     std::cout<<angle;
+        // cv::line(image,cv::Point(lines[i][0],lines[i][1]), cv::Point(lines[i][2],lines[i][3]),cv::Scalar(0, 0, 255),5,LINE_8,0);
+
+        // }
+
+        // }
+
+        imshow("lined image",image);
         
-        
+
+        //trying with findlines()
+        // findlines(masked,i);
 
         i++;
         waitKey(0); 
